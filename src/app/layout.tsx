d9478c45708e24f47d4b533210d9b412852a3554
fadhel0 <fadhel.gaaloul@gmail.com>
2024-04-15 12:26:57 +0100

    add transtions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import TransitionProvider from "@/components/TranstionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="w-screen h-screen bg-gradient-to-b">
-          <Navbar /> {children}
-        </div>
+        <TransitionProvider>{children}</TransitionProvider>
       </body>
     </html>
   );
---
+++ src/components/NavLink.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavLink = ({ link }: { link: { title: string; url: string } }) => {
+  const pathName = usePathname();
+
+  return (
+    <Link
+      className={`rounded p-1 ${
+        pathName === link.url && "bg-black text-white"
+      }`}
+      href={link.url}
+    >
+      {link.title}
+    </Link>
+  );
+};
+
+export default NavLink;
